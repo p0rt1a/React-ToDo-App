39c30c67b3354e6969bc305ea5f49e6682648719
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-07-13 19:57:38 +0300

    create todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 0,
+      title: "Task I",
+      isDone: false,
+    },
+    {
+      id: 1,
+      title: "Task II",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "Task III",
+      isDone: true,
+    },
+  ]);
+  const [filteredTasks, setFilteredTasks] = useState(tasks);
+
+  let task = {
+    id: tasks.length,
+    title: "",
+    isDone: false,
+  };
+
+  const onChange = (e) => {
+    task.title = e.target.value;
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    setTasks([...tasks, task]);
+  };
+
+  const removeTask = (id) => {
+    let newTasksList = tasks.filter((item) => item.id !== id);
+
+    setTasks(newTasksList);
+  };
+
+  useEffect(() => {
+    console.log(tasks);
+
+    setFilteredTasks(tasks);
+  }, [tasks]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>TO-DOS</h1>
       </header>
+
+      <div className="todo-list">
+        <form onSubmit={onSubmit}>
+          <input placeholder="What needs to be done?" onChange={onChange} />
+        </form>
+
+        <div className="list">
+          <ul>
+            {filteredTasks.map((item, i) => {
+              return (
+                <li key={i}>
+                  <h3>{item.title}</h3>
+                  <div className="task-operations">
+                    {!item.isDone && (
+                      <small
+                        onClick={() => {
+                          item.isDone = true;
+                          setFilteredTasks(tasks);
+                        }}
+                      >
+                        Done
+                      </small>
+                    )}
+                    <small
+                      onClick={() => {
+                        let taskList = tasks.filter(
+                          (task) => task.id !== item.id
+                        );
+
+                        setTasks(taskList);
+
+                        setFilteredTasks(tasks);
+                      }}
+                    >
+                      Remove
+                    </small>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="navigations">
+          <small
+            onClick={() => {
+              setFilteredTasks(
+                tasks.filter((item) => {
+                  return item.title.includes("");
+                })
+              );
+            }}
+          >
+            All
+          </small>
+          <small
+            onClick={() => {
+              setFilteredTasks(
+                tasks.filter((item) => {
+                  return item.isDone == false;
+                })
+              );
+            }}
+          >
+            Active
+          </small>
+          <small
+            onClick={() => {
+              setFilteredTasks(
+                tasks.filter((item) => {
+                  return item.isDone == true;
+                })
+              );
+            }}
+          >
+            Completed
+          </small>
+        </div>
+      </div>
     </div>
   );
 }
